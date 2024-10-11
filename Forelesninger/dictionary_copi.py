420b53768a45dd7916c9fb8188dbbe17240b6207
art-16 <inaaa@hiof.no>
2024-10-11 21:46:25 +0200

    testa dictionary igjen
    Skjønner ikke en dritt

--- Forelesninger/dictionary_copi.py
+++ Forelesninger/dictionary_copi.py
@@ -1,5 +1,17 @@
 #oppretter en dictonary med 3 key valuer
 brettspill = {
-    'tittel: Dixit',
-    'spilletid : 30'
+    'tittel' : "Dixit",
+    'spilletid' : 30,
+    'aldersgrense' : 8
 }
+
+#skriver ut lista
+#print(brettspill)
+#print()
+
+print(brettspill['spilletid'])
+print()
+
+#nedenfor endrer vi verdien som er i spiller til fra 30 til 40
+brettspill['spilletid'] = 40
+print((brettspill))
